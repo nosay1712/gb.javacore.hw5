f241df9e71333d86a33889250281bb99dbb86ea8
Artem <noskov.artem@gmail.com>
2021-02-19 13:30:14 +0300

    homework 5

--- src/main/java/homework5/AppData.java
+++ src/main/java/homework5/AppData.java
@@ -1,4 +1,21 @@
 package homework5;
 
-public class AppData {
+import java.io.Serializable;
+
+public class AppData implements Serializable {
+    private String[] header;
+    private int[][] data;
+
+    public AppData(String[] header, int[][] data) {
+        this.header = header;
+        this.data = data;
+    }
+
+    public String[] getHeader() {
+        return header;
+    }
+
+    public int[][] getData() {
+        return data;
+    }
 }
--- src/main/java/homework5/Main.java
+++ src/main/java/homework5/Main.java
@@ -1,4 +1,53 @@
 package homework5;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    public static void main(String[] args) throws IOException {
+        String[] header = {"Value1", "Value2", "Value3"};
+        int [][] data = {{100, 400, 600}, { 400,240,358}};
+        AppData appData = new AppData(header, data);
+        writeFile(appData);
+        readFile("file.csv");
+    }
+
+    public static void writeFile(AppData appData){
+        try(PrintWriter out = new PrintWriter("file.csv")){
+            for (String v: appData.getHeader()){
+                out.print(v + ";");
+            }
+            out.println();
+
+            int [][] valListData = appData.getData();
+            for (int i = 0;i< valListData.length; i++){
+                for ( int j =0; j < valListData[i].length; j++){
+                    out.print(valListData[i][j] + ";");
+                }
+                out.write("\r\n");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static AppData readFile (String filename) throws IOException{
+        BufferedReader in = new BufferedReader(new FileReader(filename));
+        List<int[]> data = new ArrayList<>();
+        String[] dataHead = in.readLine().split(";");
+        String line = null;
+        while ((line= in.readLine()) != null){
+            List <Integer> list = new ArrayList<>();
+            String [] string = line.split(";");
+            for (int i = 0; i < string.length; i++){
+                list.add(Integer.parseInt(string[i]));
+            }
+            data.add(list.stream().mapToInt(i -> i).toArray());
+        }
+        AppData appData = new AppData(dataHead, data.toArray(new int[0][]));
+        int [][] array = appData.getData();
+        return  appData;
+    }
 }
